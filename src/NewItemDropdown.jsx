b2099f19fc4bad93a3570d9500e6a62abc5e484a
lloydinocala <lloyd@dynamicair-care.com>
2026-07-11 08:26:00 -0400

    Update NewItemDropdown.jsx

--- src/NewItemDropdown.jsx
+++ src/NewItemDropdown.jsx
@@ -17,21 +17,21 @@ export default function NewItemDropdown({ onSelect }) {
     onSelect(mode)
   }
 
-  return (<div className="org-picker-wrap" ref={wrapRef} style={{ maxWidth: 320 }}>
-    <div className="org-picker-wrap" ref={wrapRef} style={{ maxWidth: 320 }}>
-      <button className="auth-button" style={{ width: 'auto', padding: '10px 16px', margin: 0 }} onClick={() => setOpen((o) => !o)}>
-        + New ▾
-      </button>
-      {open && (
-        <div className="org-picker-list">
-          <div className="org-picker-item" onClick={() => choose('job')}>New Job</div>
-          <div className="org-picker-item" onClick={() => choose('continueJob')}>Continue an Existing Job</div>
-          <div className="org-picker-item" onClick={() => choose('customer')}>New Customer</div>
-          <div className="org-picker-item" onClick={() => choose('property')}>New Property</div>
-          <div className="org-picker-item" onClick={() => choose('pickEstimateJob')}>New Estimate</div>
-<div className="org-picker-item" onClick={() => choose('pickInvoiceJob')}>New Invoice</div>
-        </div>
-      )}
-    </div>
-  )
+  return (
+  <div className="org-picker-wrap" ref={wrapRef} style={{ maxWidth: 320 }}>
+    <button className="auth-button" style={{ width: 'auto', padding: '10px 16px', margin: 0 }} onClick={() => setOpen((o) => !o)}>
+      + New ▾
+    </button>
+    {open && (
+      <div className="org-picker-list">
+        <div className="org-picker-item" onClick={() => choose('job')}>New Job</div>
+        <div className="org-picker-item" onClick={() => choose('continueJob')}>Continue an Existing Job</div>
+        <div className="org-picker-item" onClick={() => choose('customer')}>New Customer</div>
+        <div className="org-picker-item" onClick={() => choose('property')}>New Property</div>
+        <div className="org-picker-item" onClick={() => choose('pickEstimateJob')}>New Estimate</div>
+        <div className="org-picker-item" onClick={() => choose('pickInvoiceJob')}>New Invoice</div>
+      </div>
+    )}
+  </div>
+)
 }
