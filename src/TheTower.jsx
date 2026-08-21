9c4ade094b1c558902074b0e4de881ed84bec963
lloydinocala <lloyd@dynamicair-care.com>
2026-08-21 07:48:04 -0400

    Update TheTower.jsx

--- src/TheTower.jsx
+++ src/TheTower.jsx
@@ -1,4 +1,4 @@
-mport { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from './utils/supabase'
 import MobileNav, { isFieldAdmin } from './MobileNav'
